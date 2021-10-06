538a732e320f3bc16239d8ad6c73ec362689e0cf
Cristian Flury <cristianflury@gmail.com>
2021-10-06 15:11:11 -0300

    Ejercitación 1 SQL

---
+++ Ejercitacion_1_SQL_Alkemysql.sql
@@ -0,0 +1,22 @@
+CREATE TABLE curso (
+   codigo inT NOT NULL,
+   nombre VARCHAR(45) NOT NULL,
+   descripción VARCHAR(100),
+   turno VARCHAR(45) NOT NULL,
+   PRIMARY KEY (codigo)
+
+);
+
+ALTER TABLE curso
+ADD cupo INT;
+
+INSERT INTO curso (codigo, nombre, descripción, turno, cupo)
+values (101,"Algoritmos", "Algoritmos y Estructuras de Datos", "Mañana", 35);
+
+INSERT INTO curso (codigo, nombre, descripción, turno, cupo)
+values (102,"Matemática Discreta", "", "Tarde", 30);
+
+UPDATE curso 
+SET cupo = 25;
+
+delete FROM curso WHERE codigo=101;
